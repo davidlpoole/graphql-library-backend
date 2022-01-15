a41135b848360d40b334981d9871324232e70f80
David Poole <14233215+davidlpoole@users.noreply.github.com>
2022-01-15 15:30:13 +1300

    add an optional author param to allBooks

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -115,7 +115,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      const byAuthor = (book) => book.author === args.author
+      return books.filter(byAuthor)
+    },
     allAuthors: () => authors
   },
   Author: {
