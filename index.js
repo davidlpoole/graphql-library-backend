d7039cabe05035674a9ad70ec90635fdf0b05b85
David Poole <14233215+davidlpoole@users.noreply.github.com>
2022-01-15 17:45:58 +1300

    editauthor allows updating authors birth year

--- index.js
+++ index.js
@@ -122,6 +122,11 @@ const typeDefs = gql`
       author: String!
       genres: [String]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -166,6 +171,15 @@ const resolvers = {
       }
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
